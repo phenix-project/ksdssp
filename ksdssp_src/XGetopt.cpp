90fa51e5b5ebceb644724657a97987b9414791f6
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2010-02-16 20:22:39 +0000

    XGetop converted from dos to unix line endings

--- ksdssp_src/XGetopt.cpp
+++ ksdssp_src/XGetopt.cpp
@@ -136,60 +136,60 @@
 //
 ///////////////////////////////////////////////////////////////////////////////
 
-char	*optarg;		// global argument pointer
-int		optind = 0; 	// global argv index
+char    *optarg;                // global argument pointer
+int             optind = 0;     // global argv index
 
 int Xgetopt(int argc, char *argv[], char *optstring) {
-	static char *next = NULL;
-	if (optind == 0)
-		next = NULL;
+        static char *next = NULL;
+        if (optind == 0)
+                next = NULL;
 
-	optarg = NULL;
+        optarg = NULL;
 
-	if (next == NULL || *next == '\0') {
-		if (optind == 0)
-			optind++;
+        if (next == NULL || *next == '\0') {
+                if (optind == 0)
+                        optind++;
 
-		if (optind >= argc || argv[optind][0] != '-' || argv[optind][1] == '\0') {
-			optarg = NULL;
-			if (optind < argc)
-				optarg = argv[optind];
-			return EOF;
-		}
+                if (optind >= argc || argv[optind][0] != '-' || argv[optind][1] == '\0') {
+                        optarg = NULL;
+                        if (optind < argc)
+                                optarg = argv[optind];
+                        return EOF;
+                }
 
-		if (strcmp(argv[optind], "--") == 0) {
-			optind++;
-			optarg = NULL;
-			if (optind < argc)
-				optarg = argv[optind];
-			return EOF;
-		}
+                if (strcmp(argv[optind], "--") == 0) {
+                        optind++;
+                        optarg = NULL;
+                        if (optind < argc)
+                                optarg = argv[optind];
+                        return EOF;
+                }
 
-		next = argv[optind];
-		next++;		// skip past -
-		optind++;
-	}
+                next = argv[optind];
+                next++;         // skip past -
+                optind++;
+        }
 
-	char c = *next++;
-	char *cp = strchr(optstring, c);
+        char c = *next++;
+        char *cp = strchr(optstring, c);
 
-	if (cp == NULL || c == ':')
-		return '?';
+        if (cp == NULL || c == ':')
+                return '?';
 
-	cp++;
-	if (*cp == ':') {
-		if (*next != '\0') {
-			optarg = next;
-			next = NULL;
-		}
-		else if (optind < argc) {
-			optarg = argv[optind];
-			optind++;
-		}
-		else {
-			return '?';
-		}
-	}
+        cp++;
+        if (*cp == ':') {
+                if (*next != '\0') {
+                        optarg = next;
+                        next = NULL;
+                }
+                else if (optind < argc) {
+                        optarg = argv[optind];
+                        optind++;
+                }
+                else {
+                        return '?';
+                }
+        }
 
-	return c;
+        return c;
 }
